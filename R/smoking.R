a69a8b310785ca2f1cc7ef7529c538b2232255ea
Stiofain Fordham <stiofainf@gmail.com>
2022-12-24 23:39:17 +0000

    smoking script: add special predictors and plotting.

--- R/smoking.R
+++ R/smoking.R
@@ -1,36 +1,53 @@
-library(tidyverse)
+library(dplyr)
 library(Synth)
 
 load('smoking.rda')
 df <- as.data.frame(smoking)
 
 treated.states <- 'California'
-control.states <-  c('Rhode Island', 'Tennessee', 'Indiana', 'Nevada', 'Louisiana',
-              'Oklahoma', 'New Hampshire', 'North Dakota', 'Arkansas',
-              'Virginia', 'Illinois', 'South Dakota', 'Utah', 'Georgia',
-              'Mississippi', 'Colorado', 'Minnesota', 'Texas', 'Kentucky',
-              'Maine', 'North Carolina', 'Montana', 'Vermont', 'Iowa',
-              'Connecticut', 'Kansas', 'Delaware', 'Wisconsin', 'Idaho',
-              'New Mexico', 'West Virginia', 'Pennsylvania', 'South Carolina',
-              'Ohio', 'Nebraska')
-ignore.states <- c('Alaska', 'Arizona', 'California', 'District of Columbia',
-            'Florida', 'Hawaii', 'Maryland', 'Massachusetts', 'Michigan',
-            'New Jersey', 'New York', 'Oregon', 'Washington')
+control.states <-  c('Alabama', 'Arkansas', 'Colorado', 'Connecticut',
+                     'Delaware', 'Georgia', 'Idaho', 'Illinois', 'Indiana',
+                     'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+                     'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+                     'Nebraska', 'Nevada', 'New Hampshire', 'New Mexico',
+                     'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
+                     'Pennsylvania', 'Rhode Island', 'South Carolina',
+                     'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+                     'Virginia', 'West Virginia', 'Wisconsin', 'Wyoming')
 
 fac <- factor(df$state)
-df$state_no <- as.numeric(fac)
-df <- df %>% dplyr::filter(state == treated | state %in% control)
+df$state.no <- as.numeric(fac)
 
-dataprep(
+dataprep.out <- dataprep(
   foo = df,
   predictors = c('lnincome', 'beer', 'age15to24', 'retprice'),
   predictors.op = 'mean',
+  special.predictors = list(
+    list(which(names(df) == 'lnincome'), 1980:1988, 'mean'),
+    list(which(names(df) == 'retprice'), 1980:1988, 'mean'),
+    list(which(names(df) == 'age15to24'), 1980:1988, 'mean'),
+    list(which(names(df) == 'beer'), 1984:1988, 'mean'),
+    list(which(names(df) == 'cigsale'), 1975, 'mean'),
+    list(which(names(df) == 'cigsale'), 1980, 'mean'),
+    list(which(names(df) == 'cigsale'), 1988, 'mean')
+  ),
   dependent = 'cigsale',
-  unit.variable = 'state_no',
+  unit.variable = 'state.no',
   time.variable = 'year',
   treatment.identifier = which(levels(fac) == 'California'),
   controls.identifier = control.states,
   unit.names.variable = 'state',
-  time.predictors.prior = c(1984:1989),
-  time.optimize.ssr = c(1984:1990)
+  time.predictors.prior = c(1970:1988),
+  time.optimize.ssr = c(1970:1988),
+  time.plot = c(1970:2000)
 )
+
+synth.out <- synth(dataprep.out)
+
+path.plot(
+  dataprep.res = dataprep.out,
+  synth.res = synth.out,
+  tr.intake = 1988
+)
+
+print(setNames(control.states, synth.out$solution.w))
